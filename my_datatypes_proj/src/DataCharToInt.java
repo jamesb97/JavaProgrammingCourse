c2465441064b9851e6e1b266605297d880b9a5bf
Zdravko Bozhkov <zdravko6t5@gmail.com>
2026-01-28 01:17:23 -0500

    included lab on exploring data types

---
+++ my_datatypes_proj/src/DataCharToInt.java
@@ -0,0 +1,9 @@
+package my_datatypes_proj.src;
+
+public class DataCharToInt {
+    public static void main(String s[]) {
+        char a = 'a';
+        System.out.println("a is ");
+        System.out.println("ASCII value of a is " + (int) a);
+    }
+}
