2de2d8d35689be779e28f77079503682afe02b3b
Zdravko Bozhkov <zdravko6t5@gmail.com>
2026-02-12 16:16:20 -0500

    included directory explorer lab

---
+++ ObjectOriented/my_dir_proj/src/DirectoryExplorer.java
@@ -0,0 +1,23 @@
+package ObjectOriented.my_dir_proj.src;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class DirectoryExplorer {
+    public static void main(String s[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the file or directory with the path");
+        String fileName = scanner.nextLine();
+        File file = new File(fileName);
+        if (file.exists()) {
+            if (file.isFile()) {
+                System.out.println(fileName + " is a file");
+            } else {
+                System.out.println(fileName + " is a directory");
+            }
+        } else {
+            System.out.println(fileName + " is a not a valid file or directory");
+        }
+        scanner.close();
+    }
+}
